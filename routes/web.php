06eccdc3775380e6b576c4627e160cf1efe71298
ulum804 <alfianulum79@gmail.com>
2026-01-15 11:14:14 +0700

    tampilan login

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/login', function () {
+    return view('login.login');
+});
+Route::get('/registrasi', function () {
+    return view('login.registrasi');
+});
