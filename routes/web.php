5ec3be12225e3a22ef2e96d10bdcd2e7d36cc480
ulum804 <alfianulum79@gmail.com>
2026-01-20 10:22:09 +0700

    Update routes/web.php

--- routes/web.php
+++ routes/web.php
@@ -39,21 +39,26 @@ Route::get('staff/dashboard', function() {
 // })->middleware('auth.check');
 //     return view('warehouse.warehouse-1');
 // });
-Route::get('/kepala', function () {
-    return view('warehouse.kepala-gudang');
-});
-Route::get('/warehouse-1', function () {
-    return view('warehouse.warehouse-1');
-})->name('warehouse.1');
+// Route::get('/kepala', function () {
+//     return view('warehouse.kepala-gudang');
+// });
+// Route::get('/warehouse-1', function () {
+//     return view('warehouse.warehouse-1');
+// })->name('warehouse.1');
 
-Route::get('/warehouse-2', function () {
-    return view('warehouse.warehouse-2');
-})->name('warehouse.2');
+// Route::get('/warehouse-2', function () {
+//     return view('warehouse.warehouse-2');
+// })->name('warehouse.2');
 
-Route::get('/warehouse-3', function () {
-    return view('warehouse.warehouse-3');
-})->name('warehouse.3');
+// Route::get('/warehouse-3', function () {
+//     return view('warehouse.warehouse-3');
+// })->name('warehouse.3');
 
-Route::get('/warehouse-4', function () {
-    return view('warehouse.warehouse-4');
-})->name('warehouse.4');
+// Route::get('/warehouse-4', function () {
+//     return view('warehouse.warehouse-4');
+// })->name('warehouse.4');
+
+
+Route::get('/login', [UserController::class, 'showLoginForm'])->name('login.form');
+Route::post('/login', [UserController::class, 'login'])->name('login.store');
+Route::get('/logout', [UserController::class, 'logout'])->name('logout');
